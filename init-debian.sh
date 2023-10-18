430df0021c5250c228ae13d65fc10390408a6dfd
JSubelj <jan.subelj010@gmail.com>
2023-10-18 10:15:52 +0200

    Update init-debian.sh

--- init-debian.sh
+++ init-debian.sh
@@ -8,6 +8,12 @@ read -p "Enter the folder to install the script (default: $PWD): " install_folde
 # Use the default folder if the user doesn't provide one
 install_folder=${install_folder:-$PWD}
 
+if [ "$EUID" -ne 0 ]
+    sudo_prefix="sudo"
+else
+    sudo_prefix=""
+fi
+
 
 mkscriptsdir() {
    mkdir -p $install_folder/.scripts/bin
@@ -33,8 +39,8 @@ get_email(){
 
 # Function to install necessary packages
 install_packages() {
-    sudo apt update
-    sudo apt install -y vim sudo zsh git curl
+    $sudo_prefix apt update
+    $sudo_prefix apt install -y vim $sudo_prefix zsh git curl
     echo "Package install done"
     echo ""
 }
@@ -42,38 +48,38 @@ install_packages() {
 # Function to configure email
 configure_email() {
     get_email
-    sudo cp /etc/postfix/main.cf $install_folder/.scripts/old
+    $sudo_prefix cp /etc/postfix/main.cf $install_folder/.scripts/old
     read -p "Select Internet Site and enter your hostname. Press enter to continue..."
-    sudo apt install libsasl2-modules postfix mailutils
+    $sudo_prefix apt install libsasl2-modules postfix mailutils
     read -p "Input valid gmail sender email: " email_sender
     read -p "Generate app password and post it (https://security.google.com/settings/security/apppasswords): " app_password_email
-    sudo sh -c "echo '[smtp.gmail.com]:587 $email_sender:$app_password_email' > /etc/postfix/sasl/sasl_passwd"
-    sudo postmap /etc/postfix/sasl/sasl_passwd
-    sudo chown root:root /etc/postfix/sasl/sasl_passwd /etc/postfix/sasl/sasl_passwd.db
-    sudo chmod 0600 /etc/postfix/sasl/sasl_passwd /etc/postfix/sasl/sasl_passwd.db
-    sudo sed -i '/^relayhost =/d' /etc/postfix/main.cf
-    sudo sh -c "echo 'relayhost = [smtp.gmail.com]:587' >> /etc/postfix/main.cf"
-    sudo sh -c "echo 'smtp_sasl_auth_enable = yes' >> /etc/postfix/main.cf"
-    sudo sh -c "echo 'smtp_sasl_security_options = noanonymous' >> /etc/postfix/main.cf"
-    sudo sh -c "echo 'smtp_sasl_password_maps = hash:/etc/postfix/sasl/sasl_passwd' >> /etc/postfix/main.cf"
-    sudo sh -c "echo 'smtp_tls_security_level = encrypt' >> /etc/postfix/main.cf"
-    sudo sh -c "echo 'smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt' >> /etc/postfix/main.cf"
-    sudo systemctl restart postfix
+    $sudo_prefix sh -c "echo '[smtp.gmail.com]:587 $email_sender:$app_password_email' > /etc/postfix/sasl/sasl_passwd"
+    $sudo_prefix postmap /etc/postfix/sasl/sasl_passwd
+    $sudo_prefix chown root:root /etc/postfix/sasl/sasl_passwd /etc/postfix/sasl/sasl_passwd.db
+    $sudo_prefix chmod 0600 /etc/postfix/sasl/sasl_passwd /etc/postfix/sasl/sasl_passwd.db
+    $sudo_prefix sed -i '/^relayhost =/d' /etc/postfix/main.cf
+    $sudo_prefix sh -c "echo 'relayhost = [smtp.gmail.com]:587' >> /etc/postfix/main.cf"
+    $sudo_prefix sh -c "echo 'smtp_sasl_auth_enable = yes' >> /etc/postfix/main.cf"
+    $sudo_prefix sh -c "echo 'smtp_sasl_security_options = noanonymous' >> /etc/postfix/main.cf"
+    $sudo_prefix sh -c "echo 'smtp_sasl_password_maps = hash:/etc/postfix/sasl/sasl_passwd' >> /etc/postfix/main.cf"
+    $sudo_prefix sh -c "echo 'smtp_tls_security_level = encrypt' >> /etc/postfix/main.cf"
+    $sudo_prefix sh -c "echo 'smtp_tls_CAfile = /etc/ssl/certs/ca-certificates.crt' >> /etc/postfix/main.cf"
+    $sudo_prefix systemctl restart postfix
     echo "Mail config succeeded" | mail -s "Mail config successful" $email_address
     echo ""
 }
 
 # Function to configure unattended upgrades
 configure_unattended_upgrades() {
-    sudo apt install -y unattended-upgrades apt-listchanges
-    sudo dpkg-reconfigure -plow unattended-upgrades
+    $sudo_prefix apt install -y unattended-upgrades apt-listchanges
+    $sudo_prefix dpkg-reconfigure -plow unattended-upgrades
 
-    if sudo cat /etc/postfix/main.cf | grep -q "[smtp.gmail.com]"; then
+    if $sudo_prefix cat /etc/postfix/main.cf | grep -q "[smtp.gmail.com]"; then
         read -p "Do you want to get an email when system upgrades (y/N)? " configure_email
         if [ "$configure_email" == "Y" ] || [ "$configure_email" == "y" ]; then
             get_email
-            sudo cp /etc/apt/apt.conf.d/50unattended-upgrades $install_folder/.scripts/old
-            sudo sh -c "sed -i 's/Unattended-Upgrade::Mail \"root\";/Unattended-Upgrade::Mail \"$email_address\";/' /etc/apt/apt.conf.d/50unattended-upgrades"
+            $sudo_prefix cp /etc/apt/apt.conf.d/50unattended-upgrades $install_folder/.scripts/old
+            $sudo_prefix sh -c "sed -i 's/Unattended-Upgrade::Mail \"root\";/Unattended-Upgrade::Mail \"$email_address\";/' /etc/apt/apt.conf.d/50unattended-upgrades"
         fi
     fi
 
@@ -96,19 +102,19 @@ enable_nfs_share(){
     if [ -d "$share_dir" ] && [ -f "$share_dir/permanent.txt" ]; then
         echo "Share is mounted correctly"
     else
-           if sudo cat /proc/1/environ | tr \\0 \\n | grep -q "container=lxc"; then
+           if $sudo_prefix cat /proc/1/environ | tr \\0 \\n | grep -q "container=lxc"; then
             echo "Container is CT"
             echo "Share is not mounted correctly"
             echo "On the host, run 'pct set <id-ct> -mp0 $host_share_dir,mp=$share_dir'"
     
         else
-            sudo cp /etc/fstab $install_folder/.scripts/old
+            $sudo_prefix cp /etc/fstab $install_folder/.scripts/old
             echo "Container is VM"
-            sudo apt install nfs-common
-            sudo mkdir -p $share_dir
-            sudo sh -c "echo '$ip_host:$host_share_dir $share_dir nfs defaults 0 0' >> /etc/fstab"
-            sudo systemctl daemon-reload
-            sudo mount -a
+            $sudo_prefix apt install nfs-common
+            $sudo_prefix mkdir -p $share_dir
+            $sudo_prefix sh -c "echo '$ip_host:$host_share_dir $share_dir nfs defaults 0 0' >> /etc/fstab"
+            $sudo_prefix systemctl daemon-reload
+            $sudo_prefix mount -a
             fi
         fi
     fi
@@ -133,21 +139,21 @@ install_rcs() {
 
 undo_fstab(){
     if [ -f "$install_folder/.scripts/old/fstab" ]; then
-        sudo cp $install_folder/.scripts/old/fstab /etc/fstab
-        sudo systemctl daemon-reload
-        sudo mount -a
+        $sudo_prefix cp $install_folder/.scripts/old/fstab /etc/fstab
+        $sudo_prefix systemctl daemon-reload
+        $sudo_prefix mount -a
     fi
 }
 
 undo_postfix_main(){
     if [ -f "$install_folder/.scripts/old/main.cf" ]; then
-        sudo cp $install_folder/.scripts/old/main.cf /etc/postfix/main.cf
+        $sudo_prefix cp $install_folder/.scripts/old/main.cf /etc/postfix/main.cf
     fi
 }
 
 undo_50unattended-upgrades(){
     if [ -f "$install_folder/.scripts/old/50unattended-upgrades" ]; then
-        sudo cp $install_folder/.scripts/old/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
+        $sudo_prefix cp $install_folder/.scripts/old/50unattended-upgrades /etc/apt/apt.conf.d/50unattended-upgrades
     fi
 }
 
