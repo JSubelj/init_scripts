59c6577f483f9eb49d1e2b41f1684479b785dfd7
JSubelj <jan.subelj010@gmail.com>
2023-10-20 12:56:29 +0200

    Update init-debian.sh

--- init-debian.sh
+++ init-debian.sh
@@ -40,7 +40,7 @@ get_email(){
 # Function to install necessary packages
 install_packages() {
     $sudo_prefix apt update
-    $sudo_prefix apt install -y vim sudo zsh git curl rsync gcp
+    $sudo_prefix apt install -y vim sudo zsh git curl rsync gcp unzip
     echo "Package install done"
     echo ""
 }
